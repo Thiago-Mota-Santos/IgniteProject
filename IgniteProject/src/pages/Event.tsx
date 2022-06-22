436e8136d1f5d7e1a8c5fee25e8da9cdfa106d38
Thiago Mota Santos <lucimara1422@gmail.com>
2022-06-22 20:54:14 -0300

    commit

--- IgniteProject/src/pages/Event.tsx
+++ IgniteProject/src/pages/Event.tsx
@@ -1,13 +1,16 @@
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
 import Video from "../components/Video";
 
 const Event: React.FC = () => {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {slug ? <Video LessonSlug={slug} /> : <div className="flex-1" />}
         <SideBar />
       </main>
     </div>
